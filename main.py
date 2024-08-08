efb17041a8bdefc63630e9bb12f567fd15661a5d
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-08-08 01:17:33 +0000

    Worked on some new stuff like adding a selector for how many dice you want to roll and working on a system to add them

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
-#imports random numbers
+#imports the random numbers
 import random
 
-def rolldice():
-    b = random.randint(1, 6)
-    #custom numbers
-    if b == 1:
-        print("[-----]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[\u00A0  0 ]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[-----]")
-    elif b == 2:
-        print("[-----]")
-        print("[  0 ]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[\u00A0\u00A0  0 ]")
-        print("[-----]")
-    elif b == 3:
-        print("[-----]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[0 0 0]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[-----]")
-    elif b == 4:
-        print("[-----]")
-        print("[0 0]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[0 0]")
-        print("[-----]")
-    elif b == 5:
-        print("[-----]")
-        print("[0 0]")
-        print("[\u00A0  0 ]")
-        print("[0 0]")
-        print("[-----]")
-    else:
-        print("[-----]")
-        print("[0 0 0]")
-        print("[\u00A0\u00A0\u00A0\u00A0 ]")
-        print("[0 0 0]")
-        print("[-----]")
+def rolldice(num_dice):
+    """
+    Rolls for how many rolls you want
+    And Also selecting how many dice you want to role per base!
+    """
+    for _ in range(num_dice):
+        b = random.randint(1, 6)
+        if b == 1:
+            print("[-----]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[\u00a0  0 ]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[-----]", end=" ")
+        elif b == 2:
+            print("[-----]")
+            print("[  0 ]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[\u00a0\u00a0  0 ]")
+            print("[-----]", end=" ")
+        elif b == 3:
+            print("[-----]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[0 0 0]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[-----]", end=" ")
+        elif b == 4:
+            print("[-----]")
+            print("[0 0]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[0 0]")
+            print("[-----]", end=" ")
+        elif b == 5:
+            print("[-----]")
+            print("[0 0]")
+            print("[\u00a0  0 ]")
+            print("[0 0]")
+            print("[-----]", end=" ")
+        else:
+            print("[-----]")
+            print("[0 0 0]")
+            print("[\u00a0\u00a0\u00a0\u00a0 ]")
+            print("[0 0 0]")
+            print("[-----]", end=" ")
 
-#asks if user wants to roll again
+    total_sum = 0  # Total Sum is 0 per default
+    for _ in range(num_dice):
+            b = random.randint(1, 6)
+            total_sum += b  
+
+    print(f"Complete sum of {num_dice} dice rolls is {total_sum}!")
 def main():
+
     while True:
-        rolldice()
-        x = input("Press 'y' to roll again or 'n' to exit: ")
-        if x.lower() != 'y':
-            break
-          
-#calls the entire function using classes and functions
+        try:
+            num_dice = int(input("Enter how many dice you want to roll (0 to exit): "))
+            if num_dice == 0:
+                break
+            rolldice(num_dice)
+            print()  # After Displaying Results
+        except ValueError:
+            print("NOT VALID! ENTER A POSTIVE NUMBER OR CLICK 0 TO LEAVE!")
+
 if __name__ == "__main__":
     main()
